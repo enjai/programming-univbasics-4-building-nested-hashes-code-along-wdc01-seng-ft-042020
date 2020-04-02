5e312472a8d8847edbc24e016650c8dde38c9411
Injae Lee <ilee.injae@gmail.com>
2020-04-02 08:11:52 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,10 +20,11 @@ def bonus
    }
   }
 
+new_info = "dead"
 
 epic_tragedy[:montague][:hero][:status] = []
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+epic_tragedy[:montague][:hero][:status] = new_info
+epic_tragedy[:capulet][:heroine][:status] = new_info
 
 bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
