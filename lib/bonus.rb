611386cf7db4baaa1a2ca725ddab0e98cecb7ace
Injae Lee <ilee.injae@gmail.com>
2020-04-02 06:01:34 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,8 @@ def bonus
 
 bonus[:montague][:hero][:status] = "dead"
 bonus[:montague][:heroine][:status] = "dead"
-puts bonus
+
+bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
